f2e53c14a70879b52395e332e08192003ed4e996
Elizabeth Barongo <elizabethmoraab@gmail.com>
2025-04-10 10:35:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function stringReturn(str){
+    return str.map(text=>
+text.length
+    )
+}
+
+function EvenNum(arr){
+return arr.filter(num =>{
+    if(num%2===0){
+        console.log(num);
+    }
+})
+
+
+}
+
+EvenNum([6,5,4,3])
+
+
+
+
+
+
